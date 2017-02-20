2cad5deb2a1576dd1ec652fbd5a229ddf1e656e4
Karissa Martin <karissame.martin@gmail.com>
2017-02-20 13:23:25 -0600

    the /preview route working as expected now

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,17 @@
 'use strict';
 
 import React from 'react'
-import { Route, Router, IndexRoute } from 'react-router'
+import { Route, Router, IndexRoute, browserHistory} from 'react-router'
 import Layout from './components/Layout';
 import Previewer from './components/Previewer';
 import NotFoundPage from './components/NotFoundPage';
 //<Route path="athlete/:id" component={IndexPage}/>
 
 const routes = (
-    <Router>
-  <Route path="/" component={Layout}>
-    <IndexRoute component={Layout}/>
-    <Route path="*" component={NotFoundPage}/>
-  </Route>
-  <Route path="/preview" component={Previewer}/>
-  </Router>
+    <Router history={browserHistory}>
+      <Route path="/" component={Layout}/>
+      <Route path="/preview" component={Previewer}/>
+    </Router>
 );
 
 export default routes;
