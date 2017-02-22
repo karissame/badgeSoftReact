f1ec04faa8fd9c1c41617ed2f3606375b46fd89e
pbullion <pbullion@gmail.com>
2017-02-22 15:00:34 -0600

    added some stuff to card display that is really just for reference and don’t wanna throw away

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -58,3 +58,33 @@ export default class CardEditor extends React.Component {
 
 
 /*************/
+
+
+
+  {*/// ****************** this is the example with the red square
+// render() {
+    //     return  (
+    //     <div>
+    //         <canvas class="card-wrapper" height="610px" width="384px" id="c"></canvas>
+    //         <script src="/js/fabric.js"></script>
+    //     </div>
+    //     )
+    //
+    //
+    // };
+
+
+// render() {
+    //         if (this.props.card.id) {
+    //             var objects = [];
+    //             this.props.card.layoutObjects.forEach(function(object) {
+    //                 objects.push(<div className="objects" id={object.id} style={object.style}>{object.element}</div>);
+    //             });
+    //         return (
+    //           <div id={this.props.card.id} className="card-wrapper">
+    //             {objects}
+    //           </div>
+    //     );}
+    //   }*/}
+
+  };
