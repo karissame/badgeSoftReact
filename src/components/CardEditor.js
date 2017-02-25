ba2e54bf22443348ad15ce2592c038e80bba214f
Karissa Martin <karissame.martin@gmail.com>
2017-02-25 15:22:34 -0600

    Fixed issue with state change order

--- src/components/CardEditor.js
+++ src/components/CardEditor.js
@@ -55,9 +55,8 @@ changeDesign(event) {
       connection: connection,
       userid:this.props.user.id
     };
-    var columnarr=[];
     var designid="";
-    var design="";
+    var design='{"objects":[{"type":"rect","left":50,"top":50,"width":20,"height":20,"fill":"green","overlayFill":null,"stroke":null,"strokeWidth":1,"strokeDashArray":null,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"selectable":true,"hasControls":true,"hasBorders":true,"hasRotatingPoint":false,"transparentCorners":true,"perPixelTargetFind":false,"rx":0,"ry":0},{"type":"circle","left":100,"top":100,"width":100,"height":100,"fill":"red","overlayFill":null,"stroke":null,"strokeWidth":1,"strokeDashArray":null,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"selectable":true,"hasControls":true,"hasBorders":true,"hasRotatingPoint":false,"transparentCorners":true,"perPixelTargetFind":false,"radius":50}],"background":"rgba(0, 0, 0, 0)"}';
     var self = this;
     var userid = this.props.user.id;
     var designid = 0;
@@ -65,8 +64,8 @@ changeDesign(event) {
       .then(function (response) {
         var columns = Object.keys(response.data.message);
         var designs = response.data.designs;
-        // console.log(designs);
-        if (self.state.designs) {
+        console.log("Designs received from callback",designs);
+        if (self.state.designs[0]) {
             if (self.state.columns) {
             // var columnarr = JSON.stringify(self.state.columns);
                 console.log("Columns and design already in state ");
@@ -83,7 +82,7 @@ changeDesign(event) {
     if (self.state.designid) {
     return (
       <div>
-        <iframe id="kitchensinkiframe" className="kitchensink" src={'/kitchen/kitchensink.html?columns=' + columnarr + '&userid=' + userid + '&designid' + designid + '&design=' + design}/>
+        <iframe id="kitchensinkiframe" className="kitchensink" src={'/kitchen/kitchensink.html?columns=' + JSON.stringify(self.state.columns) + '&userid=' + userid + '&designid' + designid + '&design=' + design}/>
 
       </div>
   );} else {
