555fdef05846de700305136a6e76cef1e006c8a9
pbullion <pbullion@gmail.com>
2017-02-26 15:29:19 -0600

    added step by step pictures added example id cards for main landing page working on the step by step instructions

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -179,12 +179,58 @@ export default class Profile extends React.Component {
                     );}
                 } else {
             return (
-            <div className="row">
-            <div id="updateform">
-              <div id="profile" className="col-xs-8 col-xs-offset-2">
-                  <h1>Log in to continue or <button name="register" value="register" onClick={(e) => this.props.changePage("register")}>Register</button></h1>
+            <div className="row" id="profilerow">
+              <div id="updateformmainpage">
+                <div id="profilemainpage" className="col-xs-12">
+                    <h1>Log in to continue or <button name="register" value="register" onClick={(e) => this.props.changePage("register")}>Register</button></h1>
+                    <div id="mainpageheader" className="col-xs-12">
+                      <h1><b>Print badges straight from your browser!</b></h1>
+                    </div>
+                    <div id="profilemainpage" className="col-xs-12">
+                        <img id="cardexamplestall" src="/img/cardexamples/card1.jpg"></img>
+                        <img id="cardexamplestall" src="/img/cardexamples/card2.jpg"></img>
+                        <img id="cardexampleswide" src="/img/cardexamples/card3.jpg"></img>
+                        <img id="cardexamplestall" src="/img/cardexamples/card4.jpg"></img>
+                        <img id="cardexamplestall" src="/img/cardexamples/card5.jpg"></img>
+                    </div>
+                    <div id="mainpageheader" className="col-xs-12">
+                      <h2><b>Quick! Easy! Free!</b></h2>
+                    </div>
+                    <div id="cardinstructions" className="col-xs-12">
+                      <div id="stepbystep" className="col-xs-12">
+                        <h2><b>Connect to your database</b></h2>
+                        <ul>
+                        <li>Choose which type of database you are connecting to<li>
+                        <li>Then fill out the form with your database credentials<li>
+                        </ul>
+                        <img src="/img/stepbystep/step1.png"></img>
+                      </div>
+                      <div id="stepbystep" className="col-xs-12">
+                          <h2><b>Simple to add headshot placeholders</b></h2>
+                          <ul>
+                          <li>Click on the placeholder button next to employee picture<li>
+                          <li>It adds a default placeholder picture in the middle of the id<li>
+                          </ul>
+                          <img src="/img/stepbystep/step2.png"></img>
+
+                      </div>
+                      <div id="stepbystep" className="col-xs-12">
+                        <img src="/img/stepbystep/step3.png"></img>
+                          <h2><b>Inserting text fields is a snap</b></h2>
+                          <p></p>
+                      </div>
+                      <div id="stepbystep" className="col-xs-12">
+                        <img src="/img/stepbystep/step4.png"></img>
+                          <h2><b>No software to download or maintain</b></h2>
+                          <p></p>
+                      </div>
+                      <div id="stepbystep" className="col-xs-12">
+                          <h2><b>Print to your printer of choice 24/7</b></h2>
+                          <p></p>
+                      </div>
+                    </div>
+                </div>
               </div>
-            </div>
             </div>
         );
         }
